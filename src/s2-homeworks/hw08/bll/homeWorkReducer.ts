e192623e6a5d7bd6229ddaacb57027bdfed2ada5
alinaguseva1997 <alinagorbunova93@gmail.com>
2023-07-04 13:47:13 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,23 @@
 import {UserType} from '../HW8'
+import {elGR} from "@mui/material/locale";
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload === "up") {
+                return [...state].sort((a, b) => a.name.localeCompare(b.name))
+            }
+            if (action.payload === "down") {
+                return [...state].sort((a, b) => b.name.localeCompare(a.name))
+            }// need to fix
+            return state
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age >=18 ? el : '') // need to fix
         }
         default:
             return state

3bb61f21886eaf69a90d09dd8805294ea8a63da3
alinaguseva1997 <alinagorbunova93@gmail.com>
2024-02-01 14:53:33 +0300

    hw10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,17 +1,24 @@
+import {changeThemeIdActionType} from "../../hw12/bll/themeReducer";
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export type ActionType = LoadingActionType
+| changeThemeIdActionType
+
+export const loadingReducer = (state = initState, action: ActionType): {isLoading: boolean} => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING': {
+            return {...state, isLoading: action.isLoading}
+        }
         default:
             return state
     }
 }
 
-type LoadingActionType = {
+export type LoadingActionType = {
     type: 'CHANGE_LOADING'
     isLoading: boolean
 }
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,24 @@
+import {ActionType} from "../../hw10/bll/loadingReducer";
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ActionType): {themeId: number} => { // fix any
     switch (action.type) {
         // дописать
-
+        // case 'SET_THEME_ID' {
+        //     return
+        // }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+
+export type changeThemeIdActionType = {
+    type: 'SET_THEME_ID'
+    id: number
+}
+
+export const changeThemeId = (id: number): changeThemeIdActionType => ({ type: 'SET_THEME_ID', id }) // fix any
